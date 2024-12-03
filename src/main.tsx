3204ee03d50e9be8fa44ac079c06db6dd2fbebd8
Fanceir <fx_official@outlook.com>
2024-12-03 22:53:01 +0800

    feat: 初始化

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/root";
 
-createRoot(document.getElementById('root')!).render(
+//createBrowserRouter是一个函数，返回一个路由配置对象
+//RouterProvider是一个组件，接受一个router配置对象作为参数
+
+//router是路由的配置对象
+const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
+//BrowserRouter 还是 hashRouter
+//不需要兼容就使用 BrowserRouter
